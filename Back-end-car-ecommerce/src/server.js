c9030684d7ed4c43851ccf1ab49cc61ed7217748
Kittipong Satayanusakkul <165881791+Guitarktp@users.noreply.github.com>
2024-10-15 00:40:24 +0700

    Update server.js
    respond

--- Back-end-car-ecommerce/src/server.js
+++ Back-end-car-ecommerce/src/server.js
@@ -5,3 +5,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} 🌏`);
 });
+
+// server connection test
+app.get("/", (req, res) => {
+  res.json({ data: "respond received from the server!" });
+});
