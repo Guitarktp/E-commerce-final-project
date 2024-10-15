1f95beb114ebe88d032fc752d037eb1d6e76b73e
Kittipong Satayanusakkul <165881791+Guitarktp@users.noreply.github.com>
2024-10-15 17:05:34 +0700

    Update constants.js
    update url

--- Front-end-car-ecommerce/src/utils/constants.js
+++ Front-end-car-ecommerce/src/utils/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://localhost:5000/api";
 // export const BASE_URL = "https://back-end-car-ecommerce-5-ptu0.onrender.com/api";
-export const BASE_URL = "https://e-commerce-final-project-17g6.onrender.com/api"; 
+export const BASE_URL = "https://e-commerce-final-project-81r6.vercel.app/"; 
 
 
 
